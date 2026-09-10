4935e9c219d69f0d6c61db42a328024e2c1ed419
Per1ch <84645791+per1ch@users.noreply.github.com>
2026-09-10 18:37:21 +0300

    Update UpperHud.lua

--- [HUD] Disable Components/Lua/Client/Features/UpperHud.lua
+++ [HUD] Disable Components/Lua/Client/Features/UpperHud.lua
@@ -31,12 +31,6 @@ local function enabled()
     return ClientState.Get(KEY)
 end
 
--- Crew status panel (the upper-HUD list of crew members).
-Safe.PatchMethod("Barotrauma.CrewManager", "DrawCharacterOrder", nil, function(instance, ptable)
-    if not enabled() then return end
-    ptable.PreventExecution = true
-end, Hook.HookMethodType.Before)
-
 Safe.PatchMethod("Barotrauma.CrewManager", "AddToGUIUpdateList", nil, function(instance, ptable)
     if not enabled() then return end
     ptable.PreventExecution = true
